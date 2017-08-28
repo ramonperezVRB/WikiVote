600ac742a3e2c6dcc500242476ce0f9d4aa9af81
ramonperezVRB <ramon.perez@votingrightsbrigade.org>
2017-08-28 21:42:17 +0000

    Update example shell script to store JSON objects to file

--- apps/scripts/example1.sh
+++ apps/scripts/example1.sh
@@ -5,11 +5,13 @@ do
 OUTPUT="$(jq '.results[].bills['$number'].bill_id' ~/apps/propublica/introduced.json)"
 echo  "${OUTPUT:1:-5}"
 OUTPUT2="${OUTPUT:1:-5}"
-OUTPUT3="$(curl "https://api.propublica.org/congress/v1/115/bills/${OUTPUT2}.json" -H "X-API-Key: ${PROPUBLICA_API_KEY}" | jq '.results[].bill,.results[].title,.results[].summary')"
+#OUTPUT3="$(
+sudo curl "https://api.propublica.org/congress/v1/115/bills/${OUTPUT2}.json" -H "X-API-Key: ${PROPUBLICA_API_KEY}" -o ~/apps/propublica/bills/${OUTPUT2}.json
+jq '.results[].bill,.results[].title,.results[].summary' ~/apps/propublica/bills/${OUTPUT2}.json
 #echo "${OUTPUT3}"
 
-OUTPUT4="$(echo "${OUTPUT3}" | sed -e 's/^"//' -e 's/"$//')"
-echo "${OUTPUT4}"
+#OUTPUT4="$(echo "${OUTPUT3}" | sed -e 's/^"//' -e 's/"$//')"
+#echo "${OUTPUT4}"
 
 #curl "http://wikivote.co/api.php?action=edit&format=json&title=Test&text=article+content&summary=test+summary&basetimestamp=2007-08-24T12%3A34%3A54.000Z&token=d646e91b36d2ffaebd8f65ca8b09be1359a46b17%2B%5C"
 done
