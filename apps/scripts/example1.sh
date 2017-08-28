89f92c0890036dfca02bdf8fb6c3de029bf96bd4
ramonperezVRB <ramon.perez@votingrightsbrigade.org>
2017-08-28 21:50:19 +0000

    Storing components of each JSON as separate variables for curl call to Wiki

--- apps/scripts/example1.sh
+++ apps/scripts/example1.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-for number in {0..1}
+for number in {0..19}
 do
 
 OUTPUT="$(jq '.results[].bills['$number'].bill_id' ~/apps/propublica/introduced.json)"
@@ -7,11 +7,12 @@ echo  "${OUTPUT:1:-5}"
 OUTPUT2="${OUTPUT:1:-5}"
 #OUTPUT3="$(
 sudo curl "https://api.propublica.org/congress/v1/115/bills/${OUTPUT2}.json" -H "X-API-Key: ${PROPUBLICA_API_KEY}" -o ~/apps/propublica/bills/${OUTPUT2}.json
-jq '.results[].bill,.results[].title,.results[].summary' ~/apps/propublica/bills/${OUTPUT2}.json
-#echo "${OUTPUT3}"
-
-#OUTPUT4="$(echo "${OUTPUT3}" | sed -e 's/^"//' -e 's/"$//')"
-#echo "${OUTPUT4}"
+BILL="$(jq '.results[].bill' ~/apps/propublica/bills/${OUTPUT2}.json | sed -e 's/^"//' -e 's/"$//')"
+echo "${BILL}"
+TITLE="$(jq '.results[].title' ~/apps/propublica/bills/${OUTPUT2}.json | sed -e 's/^"//' -e 's/"$//')"
+echo "${TITLE}"
+SUMMARY="$(jq '.results[].summary' ~/apps/propublica/bills/${OUTPUT2}.json | sed -e 's/^"//' -e 's/"$//')"
+echo "${SUMMARY}"
 
 #curl "http://wikivote.co/api.php?action=edit&format=json&title=Test&text=article+content&summary=test+summary&basetimestamp=2007-08-24T12%3A34%3A54.000Z&token=d646e91b36d2ffaebd8f65ca8b09be1359a46b17%2B%5C"
 done
