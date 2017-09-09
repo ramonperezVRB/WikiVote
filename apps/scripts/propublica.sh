3294b5fbc6459c663374d0527c9bbb8c4d964619
ramonperezVRB <ramon.perez@votingrightsbrigade.org>
2017-09-09 01:35:17 +0000

    Adding pagination so we can iterate through all bills in the Propublica dataset

--- apps/scripts/propublica.sh
+++ apps/scripts/propublica.sh
@@ -6,14 +6,31 @@ SESSION=115
 CHAMBER=house
 TYPE=active
 
-#Get the most recent 20 bills introduced in Congress from the Propublica API
-sudo curl "https://api.propublica.org/congress/v1/${SESSION}/${CHAMBER}/bills/${TYPE}.json" -H "X-API-Key: ${PROPUBLICA_API_KEY}" -o ~/apps/propublica/bills/${TYPE}.json
+#Paginate through recent bills in batches of 20
+COUNT=0
+PAGINATE=20
 
-for number in {0..1}
+for NUMBER in {0..1}
+do
+
+QUERY=$((NUMBER*PAGINATE))
+echo "${QUERY}"
+
+#Get the most recent 20 bills introduced in Congress from the Propublica API
+sudo curl "https://api.propublica.org/congress/v1/${SESSION}/${CHAMBER}/bills/${TYPE}.json?offset=${QUERY}" -H "X-API-Key: ${PROPUBLICA_API_KEY}" -o ~/apps/propublica/bills/${CHAMBER}/${SESSION}/${TYPE}.json
+
+#Count the number of bills returned, keeping a running tally
+BILL_COUNT="$(jq '.results[].num_results' ~/apps/propublica/bills/${CHAMBER}/${SESSION}/${TYPE}.json)"
+COUNT=$((COUNT+BILL_COUNT))
+echo "${COUNT}"
+
+
+#Now pull the content from those 20 bills
+for number2 in {0..1}
 do
 
 #Cycle through each bill in the list  and output the bill name to a variable
-OUTPUT="$(jq '.results[].bills['$number'].bill_id' ~/apps/propublica/bills/${TYPE}.json)"
+OUTPUT="$(jq '.results[].bills['$number2'].bill_id' ~/apps/propublica/bills/${CHAMBER}/${SESSION}/${TYPE}.json)"
 
 #Trim the output to remove quotations and Congressional session indicator"
 echo  "${OUTPUT:1:-5}"
@@ -42,9 +59,9 @@ echo "${SPONSOR_ID}"
 
 #Set some wiki markup language based on the image of either the House or Senate seal
 if [ "$CHAMBER" == "house" ]; then
-	IMAGE="[[File:Seal of the United States House of Representatives.png|200x200px|${BILL} is currently pending action in the U.S. House of Representatives|thumb]]"
+	IMAGE="[[File:Seal of the United States House of Representatives.png|305x305px|${BILL} is currently pending action in the U.S. House of Representatives|thumb]]"
 elif [ "$CHAMBER" == "senate" ]; then
-	IMAGE="[[File:US-Senate-UnofficialAltGreatSeal.png|200x200px|${BILL} is currently pending action in the United States Senate|thumb]]"
+	IMAGE="[[File:US-Senate-UnofficialAltGreatSeal.png|305x305px|${BILL} is currently pending action in the United States Senate|thumb]]"
 else
 	IMAGE=" "
 fi
@@ -53,6 +70,8 @@ fi
 #Call the WikiVote API to create a new Wiki page with the given bill number, title, and summary
 . ./wiki-add.sh
 
+
+done
 
 done
 
